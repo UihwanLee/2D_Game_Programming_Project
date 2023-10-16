3dcb8ea124e66abc23d14a65273824e01e97a2b5
UihwanLee <luh5063014@naver.com>
2023-10-16 15:44:06 +0900

    Game2DGP class에 Handle_events() 함수를 추가하여 'ESC' 키를 눌렀을 때 프로그램 종료 구현

--- 2DGPGame.py
+++ 2DGPGame.py
@@ -4,10 +4,19 @@ class Game2DGP:
     def __init__(self):
         self.running = True
 
+    def Handle_events(self):
+        events = get_events()
+        for event in events:
+            if event.type == SDL_QUIT:
+                self.running = False
+            elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+                self.running = False
+
     def Run(self):
         open_canvas()
         while self.running:
             clear_canvas()
+            self.Handle_events()
 
         close_canvas()
 

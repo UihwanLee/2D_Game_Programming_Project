6c8b90983a7c00da72f57aaa5f12ed872518f573
UihwanLee <luh5063014@naver.com>
2023-10-16 15:40:36 +0900

    Game2DGP class 추가 및 pico2d 라이브러리를 활용하여 canvas를 그리기 구현

--- 2DGPGame.py
+++ 2DGPGame.py
@@ -1,3 +1,17 @@
 from pico2d import *
 
-print('Hello')
+class Game2DGP:
+    def __init__(self):
+        self.running = True
+
+    def Run(self):
+        open_canvas()
+        while self.running:
+            clear_canvas()
+
+        close_canvas()
+
+program = Game2DGP()
+program.Run()
+
+

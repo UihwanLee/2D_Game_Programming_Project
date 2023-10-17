b88b557613ab29002597466a9ac4cf92c16bcea7
UihwanLee <luh5063014@naver.com>
2023-10-17 11:52:55 +0900

    GameEngine에서 게임 실행 할 수 있도록 Run() 함수 이동

--- GameEngine.py
+++ GameEngine.py
@@ -1,11 +1,22 @@
+from pico2d import *
 from scene import Scene
+
 
 # 게임 내 모든 씬, 오브젝트, 시스템 관리할 클래스
 class GameEngine:
     def __init__(self):
+        self.running = True
         self.scene_01 = Scene(1)
         self.curScene = self.scene_01
         pass
+
+    def Handle_events(self):
+        events = get_events()
+        for event in events:
+            if event.type == SDL_QUIT:
+                self.running = False
+            elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+                self.running = False
 
     def BuildScenes(self):
         self.scene_01.CreateObject((400, 300), 'Sprites/BG_Base.png', 0, True)
@@ -13,4 +24,13 @@ class GameEngine:
 
     def RenderScenes(self):
         self.curScene.Render()
+
+    def Run(self):
+        open_canvas()
+        self.BuildScenes()
+        while self.running:
+            self.RenderScenes()
+            self.Handle_events()
+        close_canvas()
+
 

17b5c5739f35a50b373a564d95e2e0d4622d1edc
UihwanLee <luh5063014@naver.com>
2023-10-17 10:30:18 +0900

    Scene Render를 GameEngine에서 처리 할 수 있도록 변경

--- 2DGPGame.py
+++ 2DGPGame.py
@@ -1,28 +1,11 @@
-from pico2d import *
-
 from gameEngine import GameEngine
 
 class Game2DGP:
     def __init__(self):
-        self.running = True
         self.gameEngine = GameEngine()
 
-    def Handle_events(self):
-        events = get_events()
-        for event in events:
-            if event.type == SDL_QUIT:
-                self.running = False
-            elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-                self.running = False
-
     def Run(self):
-        open_canvas()
-        self.gameEngine.BuildScenes()
-        while self.running:
-            self.gameEngine.RenderScenes()
-            self.Handle_events()
-
-        close_canvas()
+        self.gameEngine.Run()
 
 
 program = Game2DGP()

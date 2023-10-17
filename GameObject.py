b416f8a8d1aa25b132668bb387a1d7e654ec0c64
UihwanLee <luh5063014@naver.com>
2023-10-17 09:18:24 +0900

    GameObject 클래스 추가

---
+++ GameObject.py
@@ -0,0 +1,11 @@
+class GameObject:
+    def __init__(self, order, pos, layout):
+        self.pos = pos
+        self.layout = layout
+        pass
+
+    def update(self):
+        pass
+
+    def Draw(self):
+        pass

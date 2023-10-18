e967d884cc02d2199245e25ce29c38b4e22586ef
Lee Ui Hwan <luh5063014@naver.com>
2023-10-18 20:42:49 +0900

    2DGP의 여러 변수, 정보를 저장한 Define.py 추가

---
+++ Define.py
@@ -0,0 +1,14 @@
+'''
+    2DGPGame의 define 하기 위한 모듈
+    게임 오브젝트 정보, 다른 객체의 정보를 저장한다.
+'''
+
+#Object Type
+STATIC = 0
+DYNAMIC = 1
+
+#BackGround
+background_pos = (400, 300)
+background_img = 'Sprites/BG_Base.png'
+background_type = STATIC
+

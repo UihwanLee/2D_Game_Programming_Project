352804fab1c9a3cd7fbb2ddf74751e68cbb9af6c
UihwanLee <luh5063014@naver.com>
2023-10-19 14:25:30 +0900

    Player가 수행할 애니메이션 (타자/투수) 리스트 변수를 멤버 변수로 설정하여 GameSystem에서 이를 처리하도록 구조 변경.

--- gamePlayer.py
+++ gamePlayer.py
@@ -9,12 +9,14 @@ from gameObject import *
 
 class Player(GameObject):
 
-    # Player 클래스 초기화. 상속 받은 GameObject 클래스 초기화. player의 frame, time, action을 초기화 한다.
+    # Player 클래스 초기화. 상속 받은 GameObject 클래스 초기화.
+    # player의 frame, time, action, player_Anim 초기화 한다.
     def __init__(self, scene, pos, sprite, type, layer, bActive, frame):
         super().__init__(scene, pos, sprite, type, layer, bActive)
         self.frame = frame
         self.time = 0
         self.action = 0
+        self.player_Anim = player_Anim
 
     # player 업데이트. time 변수를 기준으로 각족 이벤트를 처리한다.
     def update(self):
@@ -24,7 +26,7 @@ class Player(GameObject):
         self.time += 1
 
         # 애니메이션의 다이나믹을 위해 delay : delay 함수를 호출하면 Frame이 떨어지므로 time으로 구현
-        time = player_Anim[self.action].delay[self.frame]
+        time = self.player_Anim[self.action].delay[self.frame]
         if self.time > time:
             self.frame = (self.frame + 1) % 3
             self.time = 0
@@ -36,5 +38,5 @@ class Player(GameObject):
 
         pos = super().get_object_var('pos')
         sprite = super().get_object_var('sprite')
-        posX, posY = player_Anim[self.action].posX[self.frame], player_Anim[self.action].posY[self.frame]
+        posX, posY = self.player_Anim[self.action].posX[self.frame], self.player_Anim[self.action].posY[self.frame]
         sprite.clip_draw(self.frame * 100, 0, 100, 100, posX, posY, 300, 300)
